14de72bc6b6cf5e492786cb078dff1ea14f52fe8
MusawerMehdii <115610075+MusawerMehdii@users.noreply.github.com>
2026-02-19 07:32:53 +0500

    Add files via upload

---
+++ backend/Services/ICompanyService.cs
@@ -0,0 +1,10 @@
+﻿using CleanCompanyAPi.Models;
+
+namespace CleanCompanyAPi.Services
+{
+    public interface ICompanyService
+    {
+        Task<List<CompanyDto>> SearchCompaniesAsync(string query);
+        Task<CompanyDto?> GetByOrgNumberAsync(string orgnr);
+    }
+}
